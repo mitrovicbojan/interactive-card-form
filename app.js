d2c365340572d071e8ae48efd5c9d032c2058cb3
unknown <mitrovicbojan87@gmail.com>
2023-04-03 21:27:49 +0300

    style

---
+++ app.js
@@ -0,0 +1,42 @@
+// const myObj = [
+//   {
+//     name: "John",
+//     age: 15,
+//   },
+//   {
+//     name: "Tom",
+//     age: 25,
+//   },
+// ];
+
+// for (let i in myObj) {
+//   console.log(myObj[i].name);
+// }
+
+// function inMyObj(name) {
+//   for (let i in myObj) {
+//     if (name == myObj[i].name) {
+//       return console.log("Ima u objektu");
+//     } else {
+//       return console.log("Nema u objektu");
+//     }
+//   }
+// }
+// console.log(inMyObj("John"));
+// const myShip = [
+//   {
+//     name: "Disharmony",
+//     cabinA: {
+//       price: 1000,
+//       num: 10,
+//     },
+//     cabinB: {
+//       price: 500,
+//       num: 3,
+//     },
+//   },
+// ];
+
+// for (let i in myShip) {
+//   console.log(myShip[i].cabinA.price);
+// }
