19e4aa8067c62a6abebcea5cec2799ca0a9e905d
unknown <mitrovicbojan87@gmail.com>
2023-09-20 22:22:40 +0300

    card

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const cardNumInput = document.getElementsByClassName(".num-input");
-const cardNumOutput = document.getElementsByClassName(".card-front-number");
+const cardNumOutput = document.getElementById("card-front-number");
 const noNumInput = document.getElementById("no-num-input");
 const noExpYear = document.getElementById("no-exp-year");
 const noCvcNum = document.getElementById("no-cvc-num");
@@ -7,3 +7,9 @@ const noCvcNum = document.getElementById("no-cvc-num");
 noExpYear.style.display = "None";
 noNumInput.style.display = "None";
 noCvcNum.style.display = "None";
+
+function func() {
+  let helloCard = "4566 7899 1233";
+  cardNumOutput.innerText = helloCard;
+  console.log(cardNumOutput);
+}
