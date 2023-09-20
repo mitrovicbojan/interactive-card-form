cec8cfebb1d14d89d458749b37078ca60d9f3308
unknown <mitrovicbojan87@gmail.com>
2023-09-20 22:12:26 +0300

    ids

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const cardNumInput = document.getElementsByClassName(".num-input");
 const cardNumOutput = document.getElementsByClassName(".card-front-number");
+const noNumInput = document.getElementById("no-num-input");
+const noExpYear = document.getElementById("no-exp-year");
+const noCvcNum = document.getElementById("no-cvc-num");
+
+noExpYear.style.display = "None";
+noNumInput.style.display = "None";
+noCvcNum.style.display = "None";
