5ce6a2f894752419a6888a978e3a7aa68e98964c
unknown <mitrovicbojan87@gmail.com>
2023-09-23 22:05:58 +0300

    outputs

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const noNumInput = document.getElementById("no-num-input");
 const noExpYear = document.getElementById("no-exp-year");
 const noCvcNum = document.getElementById("no-cvc-num");
 const cardName = document.getElementById("card-name");
-const expYear = document.getElementById("exp-month");
+const cardFrontName = document.getElementById("card-front-name");
+const expYear = document.getElementById("exp-year");
+const expYearOutput = document.getElementById("year");
 const expMonth = document.getElementById("exp-month");
+const expMonthOutput = document.getElementById("month");
 const cvc = document.getElementById("card-back-cvc");
+const cvcInput = document.getElementById("cvc-num");
+let cardNumInput = document.getElementById("num-input-first");
 noExpYear.style.display = "None";
 noNumInput.style.display = "None";
 noCvcNum.style.display = "None";
 
 function func() {
-  let cardNumInput = document.getElementById("num-input-first");
-
-  let card = cardNumInput.value;
-
-  cardNumOutput.innerText = card;
+  cardNumOutput.innerText = cardNumInput.value;
+  cardFrontName.innerText = cardName.value;
+  expYearOutput.innerText = expYear.value;
+  expMonthOutput.innerText = expMonth.value;
+  cvc.innerText = cvcInput.value;
 }
